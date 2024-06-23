915706b2b1e4ade72f32967424a2d5b9f7e01212
ebarbieribric <edmundo.barbieri@bric.finance>
2024-06-23 16:51:12 -0300

    Manual, ERC20 token. Tests

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+    uint256 public constant TOKEN_SUPPLY = 100 ether;
+
+    function run() external returns(OurToken) {
+        vm.startBroadcast();
+        OurToken ourToken = new OurToken(TOKEN_SUPPLY);
+        vm.stopBroadcast();
+        return ourToken;
+    }
+}
